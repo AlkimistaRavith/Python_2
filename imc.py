44a25b645e0a2c32ef32a262a87b92b77c062017
AlkimistaRavith <ravith.d3@gmail.com>
2025-05-18 16:02:52 +0000

    juego completo. se agregan condicionales para validacion

--- imc.py
+++ imc.py
@@ -2,25 +2,30 @@ try:
     import sys
     import math
 
+    #Este bloque es para determinar si se ingresa un argumento al ejecutar el programa, o si pide opción si no se ingresó argumento.
     if len(sys.argv) == 1:
-
         W = float(input("Ingrese su peso en Kiligramos: "))
         H = float(input("Ingrese su altura en Centímetros: "))
 
     elif len(sys.argv) == 3 :
-        W = sys.argv[1]
-        H = sys.argv[2]
-
-        W = float(W)
-        H = float(H)
-
+        W = float(sys.argv[1])
+        H = float(sys.argv[2])
+    #Si no se ingresan la  cantidad de argumentos válidos se evalúa IMC = 0 (solo se agrega 1 argumento o sobre 2 argumentos)
     else :
         W = 0
         H = 1
+    
+    #Condicional para validar que el ingreso se realizó en Kg o cm.
+    if W > 1000 or H < 5 :
+        W = 1
+        H = 1
 
+    #Cálculo de IMC
     IMC = W / ( (H/100) ** 2)
-
-    if IMC == 0 :
+    #Evaluación del IMC
+    if IMC == 10000 :
+        print("Has ingresado un valor no válido para tu peso o altura \n Recuerda ingresar tu peso en Kg y tu altura en cm.")
+    elif IMC == 0 :
         print("Error en el ingreso. \nPuedes ingrersar <python imc.py>, o \n <python imc.py [peso en Kg] [altura en cm]")
     elif IMC < 18.5 :
         print(f"Su IMC es {IMC:.2f}. \nLa clasificación OMS es Bajo Peso")
--- juego.py
+++ juego.py
@@ -1,46 +1,37 @@
 
 import random
+import sys
 
-opciones = ["caballero","dragon","hechicera"]
-ganadas = 0
-perdidas = 0
-empates = 0
+opciones = ["piedra","papel","tijeras"]
 
-while True:
-    jugador = input("Elige caballero, dragon o hechicera, (escribe salir para terminar el juego): ")
+#Este bloque es para determinar si se ingresa un argumento al ejecutar el programa, o si pide opción si no se ingresó argumento.
+if len(sys.argv) == 2 :
+    jugador = sys.argv[1]
     jugador = jugador.lower()
 
-    if jugador == "salir":
-        print("Juego terminado")
-        print(f"El resumen de la partida es: \n Victorias: {ganadas} \n Derrotas: {perdidas} \n Empates: {empates}")
-        break
-    if jugador not in opciones:
-        print("Esta opción no es válida.")
-        continue
+elif len(sys.argv) == 1 : 
+    jugador = input("Escoge tu opción: piedra, papel o tijeras: ")
+    jugador = jugador.lower()
+else :
+    print("Esta opción no es válida. \n Puedes escribir <python juego.py>, o \n <python juego.py [piedra, papel o tijeras]>")
 
+#Bloque para determinar si la opción es válida o si se ejecuta el juego (escoge el PC)
+if jugador not in opciones:
+    print("Esta opción no es válida. \n Puedes escribir <python juego.py>, o \n <python juego.py [piedra, papel o tijeras]>")
+else :
     computador = random.choices(opciones)
     computador = computador[0]
 
-    print(f"El computador a escogido: {computador}")
-
+    print(f"Tu jugaste {jugador}")
+    print(f"El computador jugó {computador}")
+    #Bloque dentro del bloque de validación, para ejecutar el juego y determianr ganador, perdedor o empate
     if jugador == computador:
-        print("Es un empate! La batalla continua...")
-        empates += 1
+        print("Es un empate!")
 
-    elif (jugador == "hechicera" and computador == "dragon") or \
-         (jugador == "dragon" and computador == "caballero") or \
-         (jugador == "caballero" and computador == "hechicera"):
-         print("Has ganado la batalla!")
-         ganadas += 1
+    elif (jugador == "piedra" and computador == "tijeras") or \
+            (jugador == "tijeras" and computador == "papel") or \
+            (jugador == "papel" and computador == "piedra"):
+            print("Ganaste!")
+
     else:
         print("Has perdido, intentalo de nuevo!")
-        perdidas += 1
-
-
-""" 
-import sys
-
-if len(sys.argv) > 2:
-    print("python juego.py [tu nombre]")
-else:
- """
