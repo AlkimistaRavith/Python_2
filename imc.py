dde0f9ecec0eece53a6e634a30cd311757ae1d43
AlkimistaRavith <ravith.d3@gmail.com>
2025-05-18 15:19:07 +0000

    programa imc.py compelto

--- imc.py
+++ imc.py
@@ -1,13 +1,39 @@
-import math
+try:
+    import sys
+    import math
 
-W = float(input("Ingrese su peso en Kiligramos: "))
-H = float(input("Ingrese su altura en Centímetros: "))
+    if len(sys.argv) == 1:
 
-IMC = W / ( (H/100) ** 2)
+        W = float(input("Ingrese su peso en Kiligramos: "))
+        H = float(input("Ingrese su altura en Centímetros: "))
 
-if IMC < 18.5:
-    print(f"Su IMC es {IMC}, tienes una condición de Bajo Peso.")
-if IMC < 18.5:
-    print(f"Con un IMC de {IMC}, tienes una condición de Bajo Peso.")
+    elif len(sys.argv) == 3 :
+        W = sys.argv[1]
+        H = sys.argv[2]
 
-print(f"tu IMC es: {IMC:.1f}")
+        W = float(W)
+        H = float(H)
+
+    else :
+        W = 0
+        H = 1
+
+    IMC = W / ( (H/100) ** 2)
+
+    if IMC == 0 :
+        print("Error en el ingreso. \nPuedes ingrersar <python imc.py>, o \n <python imc.py [peso en Kg] [altura en cm]")
+    elif IMC < 18.5 :
+        print(f"Su IMC es {IMC:.2f}. \nLa clasificación OMS es Bajo Peso")
+    elif IMC >= 18.5 and IMC < 25 :
+        print(f"Su IMC es {IMC:.2f}. \nLa clasificación OMS es Adecuado")
+    elif IMC >= 25 and  IMC < 30 :
+        print(f"Su IMC es {IMC:.2f}. \nLa clasificación OMS es Sobrepeso")
+    elif IMC >= 30 and IMC < 35 :
+        print(f"Su IMC es {IMC:.2f}. \nLa clasificación OMS es Obesidad Grado I")
+    elif IMC >= 35 and IMC < 40 :
+        print(f"Su IMC es {IMC:.2f}. \nLa clasificación OMS es Obesidad Grado II")
+    else :
+        print(f"Su IMC es {IMC:.2f}. \nLa clasificación OMS es Obesidad Grado III")
+
+except ValueError:
+    print("Error en el ingreso. \nPuedes ingrersar <python imc.py>, o \n <python imc.py [peso en Kg] [altura en cm]")
--- juego.py
+++ juego.py
@@ -1,7 +1,46 @@
+
+import random
+
+opciones = ["caballero","dragon","hechicera"]
+ganadas = 0
+perdidas = 0
+empates = 0
+
+while True:
+    jugador = input("Elige caballero, dragon o hechicera, (escribe salir para terminar el juego): ")
+    jugador = jugador.lower()
+
+    if jugador == "salir":
+        print("Juego terminado")
+        print(f"El resumen de la partida es: \n Victorias: {ganadas} \n Derrotas: {perdidas} \n Empates: {empates}")
+        break
+    if jugador not in opciones:
+        print("Esta opción no es válida.")
+        continue
+
+    computador = random.choices(opciones)
+    computador = computador[0]
+
+    print(f"El computador a escogido: {computador}")
+
+    if jugador == computador:
+        print("Es un empate! La batalla continua...")
+        empates += 1
+
+    elif (jugador == "hechicera" and computador == "dragon") or \
+         (jugador == "dragon" and computador == "caballero") or \
+         (jugador == "caballero" and computador == "hechicera"):
+         print("Has ganado la batalla!")
+         ganadas += 1
+    else:
+        print("Has perdido, intentalo de nuevo!")
+        perdidas += 1
+
+
+""" 
 import sys
 
 if len(sys.argv) > 2:
     print("python juego.py [tu nombre]")
 else:
-    nombre = sys.argv[1]
-    print(f"hola {nombre}")
+ """
